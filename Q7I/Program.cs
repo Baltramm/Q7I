6ed5cdad3311641859b57aeaab3369edb853d217
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-26 23:17:33 +0600

    V1.1

--- Q7I/Program.cs
+++ Q7I/Program.cs
@@ -12,7 +12,7 @@ namespace Q7I
 
     abstract class Delivery
     {
-        public string Address;
+        public Adress Address;
         public string Name;
         public string Lastname;
     }
@@ -38,16 +38,12 @@ namespace Q7I
     {
         public Adress Adress { get; set; }
 
+        static Adress[] adresses = {new Adress("г.Пермь","ул.Пушкина"), new Adress("г.Пермь", "ул.Толстого"), new Adress("г.Пермь", "ул.Шамиля") };
         public void SetAdress()
         {
-            Console.WriteLine("Введите город доставки:");
-            Adress.city = Console.ReadLine();
-            Console.WriteLine("Введите улицу доставки:");
-            Adress.street = Console.ReadLine();
-            Console.WriteLine("Введите номер дома доставки:");
-            Adress.houseNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите номер квартиры доставки:");
-            Adress.apprtNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Выб");
+            
         }
     }
 
@@ -57,20 +53,15 @@ namespace Q7I
 
         public void SetAdress()
         {
-            Console.WriteLine("Введите город доставки:");
-            Adress.city = Console.ReadLine();
-            Console.WriteLine("Введите улицу доставки:");
-            Adress.street = Console.ReadLine();
-            Console.WriteLine("Введите номер дома доставки:");
-            Adress.houseNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите номер квартиры доставки:");
-            Adress.apprtNum = Convert.ToInt32(Console.ReadLine());
+ 
         }
     }
 
-    class Order<TDelivery,TStruct> where TDelivery : Delivery
+    class Order<TDelivery, TStruct, TPayment> where TDelivery : Delivery where TPayment : Payment
     {
         public TDelivery Delivery;
+
+        public TPayment Payment;
 
         public int Number;
 
@@ -86,11 +77,32 @@ namespace Q7I
         }
         public void DisplayUserInfo()
         {
-            Console.WriteLine("Имя заказчика:{0},\bФамилия заказчика:{1}",Delivery.Name,Delivery.Lastname);
+            Console.WriteLine("Имя заказчика:{0},\bФамилия заказчика:{1}", Delivery.Name, Delivery.Lastname);
         }
 
+        public void DisplayNum()
+        {
+            Console.WriteLine(Number);
+        }
+
+        public Products[] Products = {};
         // ... Другие поля
     }
+
+    class Product
+    {
+        public string Name;
+        public string Description;
+        public string Price;
+
+
+    }
+    class Products:Product
+    {
+        public int Count;
+    }
+
+
     public class Adress
     {
         private string City;
@@ -99,20 +111,63 @@ namespace Q7I
         private int ApprtNum;
         public string city { get { return City; } set { City = value; } }
         public string street { get { return Street; } set { Street = value; } }
-        public int houseNum { get { return HouseNum; } set { HouseNum = value; } }
-        public int apprtNum { get { return ApprtNum; } set { ApprtNum = value; } }
+        public int? houseNum { get { return HouseNum; } set { HouseNum = (int)value; } }
+        public int? apprtNum { get { return ApprtNum; } set { ApprtNum = (int)value; } }
 
-        public Adress()
+        public Adress(string city, string street)
         {
-           
+            this.city = city;
+            this.street = street;
         }
 
-        public Adress(string city,string street,int housenum,int apprtnum)
+        public Adress(string city,string street,int? housenum,int? apprtnum)
         {
             this.city = city;
             this.street = street;
             houseNum = housenum;
             apprtNum = apprtnum;
         }
+    }
+    enum Status
+    {
+        NotConfirmed, Confirmed, Canlseled
+    }
+    abstract class Payment
+    {
+        public Status Status;
+
+       public abstract void PayInfo();
+        
+    }
+    abstract class CardPayment:Payment
+    {
+        public string Number1;
+        private string _number2;
+        public string Number2 { get { return "***"; } set { _number2 = value; } }
+        public override void PayInfo()
+        {
+            Console.WriteLine("Введите номер вашей карты");
+            Number1 = Console.ReadLine();
+            Console.WriteLine("Введите CVC код");
+            _number2 = Console.ReadLine();
+        }
+    }
+    class QiWi : Payment
+    {
+        public string Number;
+        public override void PayInfo()
+        {
+            Console.WriteLine("Введите номер вашего кошелька");
+            Number = Console.ReadLine();
+        }
+    }
+
+    class MasterCard :CardPayment
+    {
+       
+    }
+    class Visa : CardPayment
+    {
+      
     }
 }
